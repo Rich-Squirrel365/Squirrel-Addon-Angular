40bfcaa3fd845a9bad4cdca588d2250713717e61
Rich Harvey <rharvey@infosol.com>
2022-02-16 10:19:23 +0000

    Set up routing and removed placeholder text

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddonComponent } from './addon/addon.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AddonComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
