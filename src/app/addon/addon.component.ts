c02583f34adbf8d8d2e163ae223198ea48d203cd
Rich Harvey <rharvey@infosol.com>
2022-03-04 11:19:42 +0000

    Updated defaults

--- src/app/addon/addon.component.ts
+++ src/app/addon/addon.component.ts
@@ -8,6 +8,8 @@ import { SquirrelHelper } from '../squirrel-helper/squirrel-helper';
 })
 export class AddonComponent extends SquirrelHelper implements OnInit {
 
+  helloWorldText: string;
+
   constructor() { super(); }
 
   ngOnInit(): void {
@@ -21,17 +23,30 @@ export class AddonComponent extends SquirrelHelper implements OnInit {
   override onInitState(state: any): void {
     super.onInitState(state);
     if (state != null) {
-      
+      this.processData(state.helloWorldData);
     }
   }
 
   override onPropertyChange(property: any, value: any): void {
     super.onPropertyChange(property, value);
-    
+    switch (property) {
+      case 'helloWorldData':
+        this.processData(value);
+        break;
+    }
   }
-
 
   override onPropertyChangesComplete(): void {
     super.onPropertyChangesComplete();
+  }
+
+  /**
+   * Take a string, to be shown in the HTML display, reverse the 
+   * order of letters and then send back to Squirrel
+   * @param value The string to display and return
+   */
+  processData(value: string) {
+    this.helloWorldText = value;
+    this.sendToSquirrel('helloWorldResponse', value.split('').reverse().join(''));
   }
 }
